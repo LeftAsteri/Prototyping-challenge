7932c401dc1bf02ca4c03073c4bb171877646e08
Tiny <wisptheproto@gmail.com>
2025-11-17 15:39:26 -0800

    Original File

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,40 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
+}
+
+
+
+Future<void> sendWebhook(String content) async {
+  final String webhookUrl =
+      'https://discordapp.com/api/webhooks/1440119165116747806/kcTs3ji_UpM-IqfDV3I9tx5H9DJBTJrUyIyxpe5PYUjrpZ_Z1CaOnCyvkSR-ESISks_A';
+  
+  Map<String, dynamic> discordMessage = {
+    'content': content,
+    'username': 'Flutter App',
+  };
+
+  try {
+    final response = await http.post(
+      Uri.parse(webhookUrl),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode(discordMessage),
+    );
+
+    if (response.statusCode == 204) {
+      print('Discord webhook sent successfully!');
+    } else {
+      print(
+        'Failed to send Discord webhook: ${response.statusCode} - ${response.body}',
+      );
+    }
+  } catch (e) {
+    print('Error sending Discord webhook: $e');
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -33,7 +66,10 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ElevatedButton(onPressed: () => print('Hello, Flutter Training!'), child: Text('Print!')),
+            ElevatedButton(
+              onPressed: () => sendWebhook('If you can see this Sen is really gay'),
+              child: Text('Print!'),
+            ),
           ],
         ),
       ),
